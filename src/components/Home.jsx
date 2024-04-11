dfe184dcd4a754428a9b5bcf5e2c50b8a82dbafb
Carlos Alarcon <110437118+carlalap@users.noreply.github.com>
2024-04-11 23:51:21 +0000

    updated home file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,10 +22,9 @@ const Home = () => {
                     </h2>
                     {/* Description */}
                     <p className="text-gray-400 py-4 max-w-md">
-                    I am a future Full Stack Web Developer at Atlas School 
-                    (formerly Holberton School) in Tulsa, OK. 
-                    Take a look of my first web using
-                    technologies like React and Tailwind.
+                    I am a software engineer at Atlas School
+                    (formerly Holberton School) in Tulsa, OK. Take a look at my
+                    first website using technologies like React and Tailwind.
                     </p>
                     {/* Portfolio link */}
                     <div>
