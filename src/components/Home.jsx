9648594ccebe40b039b00c94f4909305dca67f88
Carlos Alarcon <110437118+carlalap@users.noreply.github.com>
2024-04-25 20:43:16 +0000

    class image updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     return (
         // Home section with a gradient background
         <div name="home" className="h-screen w-full bg-gradient-to-b 
-        from-black via-black to-gray-800"
+        from-black via-black to-gray-800 mt-10"
         >
             {/* Container for centering content */}
             <div className="max-w-screen-lg mx-auto flex flex-col
